186406da8e5d38399b751bfd4cb50d0f78356f3c
Osher <osher.filter@gmail.com>
2020-05-26 21:01:48 +0300

    feat(claim) - add claim.isExcoFactory

--- test/lib.claim.test.js
+++ test/lib.claim.test.js
@@ -10,6 +10,7 @@ describe('lib/claim', () => {
         describe('form', () => {
             SUT(SUT).hasApi({
                 isFunction: 2,
+                isExcoFactory: 1,
                 hasApi: 2,
                 hasProps: 2,
                 behaviors: 3,
@@ -30,6 +31,7 @@ describe('lib/claim', () => {
                         expect: {
                             api: [
                                 'isFunction',
+                                'isExcoFactory',
                                 'hasApi',
                                 'hasProps',
                                 'behaviors',
@@ -73,6 +75,304 @@ describe('lib/claim', () => {
                     expect: {},
                 },
             ]));
+        });
+
+        describe('.isExcoFactory(SUT, [opts])', () => {
+
+            const orig = { ...global };
+            const hijack = ctx => {
+                ctx.tests = [];
+                global.describe = (ttl, f) => f();
+                global.it = (ttl, f) => {
+                    ctx.tests.push({ id: ctx.tests.length, ttl, f});
+                };
+            };
+            const restore = ctx => {
+                ['describe', 'it'].forEach(api => global[api] = orig[api]);
+            };
+            const runSuite = ctx => {
+                ctx.tests.some(test => {
+                    try {
+                        test.f();
+                    } catch(e) {
+                        test.err = e;
+                        ctx.failed = test;
+                        return true;
+                    }
+                    return false;
+                });
+            }
+            const onReady = ctx => {
+                restore();
+                runSuite(ctx);
+            }
+
+            describe('when called with a non-function value', () => {
+                const ctx = SUT(SUT.isExcoFactory).behavior({
+                    before: hijack,
+                    args: ['not-a-function'],
+                    ready: onReady,
+                });
+
+                describe('the created suite', () => {
+                    it('should fail about the non-function value', () => {
+                        Should(ctx.failed)
+                            .be.an.Object()
+                            .have.property('id', 0);
+                        Should(ctx.failed.err)
+                            .have.property('message').match(/expected 'not-a-function' to be a function/);
+                    });
+                });
+            });
+
+            describe('when called with a function with wrong arity without overriding arity option', () => {
+                const ctx = SUT(SUT.isExcoFactory).behavior({
+                    before: hijack,
+                    args: [(a,b,c) => {}],
+                    ready: onReady,
+                });
+
+                it('should create one test', () => {
+                    Should(ctx.tests).property('length', 1);
+                });
+
+                describe('the created suite', () => {
+                    it('should fail about the mismatching arity', () => {
+                        Should(ctx.failed)
+                        .be.an.Object()
+                        .have.property('id', 0);
+                        Should(ctx.failed.err)
+                        .have.property('message').match(/have property length of 2 \(got 3\)/);
+                    });
+                });
+            });
+
+            describe('when called with options.arity', () => {
+                describe('and arity option is illegal (not between 0 and 2)', () => {
+                    const ctx = SUT(SUT.isExcoFactory).behavior({
+                        before: hijack,
+                        args: [(a,b,c) => {}, { arity: 3 }],
+                        ready: onReady,
+                        expect: { reject: /arity must be between 0 to 2/}
+                    });
+
+                    it('should not create any tests', () => {
+                        Should(ctx.tests).eql([]);
+                    });
+                });
+
+                describe('and arity: 1 with function with matching arity', () => {
+                    const ctx = SUT(SUT.isExcoFactory).behavior({
+                        before: hijack,
+                        args: [(a) => {}, {arity: 1}],
+                        ready: onReady,
+                    });
+
+                    it('should create one test', () => {
+                        Should(ctx.tests).property('length', 1);
+                    });
+
+                    describe('the created suite', () => {
+                        it('should pass', () => {
+                            Should.not.exist(ctx.failed);
+                        });
+                    });
+                });
+
+                describe('and arity: 0 with function with matching arity', () => {
+                    const ctx = SUT(SUT.isExcoFactory).behavior({
+                        before: hijack,
+                        args: [() => {}, {arity: 0}],
+                        ready: onReady,
+                    });
+
+                    it('should create one test', () => {
+                        Should(ctx.tests).property('length', 1);
+                    });
+
+                    describe('the created suite', () => {
+                        it('should pass', () => {
+                            Should.not.exist(ctx.failed);
+                        });
+                    });
+                });
+            });
+            describe('when called with options.validator', () => {
+                describe('and provided value has no validator', () => {
+                    const factoryWithNoValidator = (a, b) => ({ a, b });
+
+                    const ctx = SUT(SUT.isExcoFactory).behavior({
+                        before: hijack,
+                        args: [factoryWithNoValidator, { hasValidator: true }],
+                        ready: onReady,
+                    });
+
+                    it('should create two tests', () => {
+                        Should(ctx.tests).property('length', 2);
+                    });
+
+                    describe('the created suite', () => {
+                        it('should fail on the missing validator', () => {
+                            Should(ctx.failed)
+                                .be.an.Object();
+                            Should(ctx.failed.err)
+                                .have.property('message').match(/expected .+ to have type function/);
+                        });
+                    });
+                });
+
+                describe('and provided value has .validate which is not a function', () => {
+                    const factoryWithNonFuncValidate = (a, b) => ({ a, b });
+                    factoryWithNonFuncValidate.validate = 'not-a-function';
+
+                    const ctx = SUT(SUT.isExcoFactory).behavior({
+                        before: hijack,
+                        args: [factoryWithNonFuncValidate, { hasValidator: true }],
+                        ready: onReady,
+                    });
+
+                    it('should create two tests', () => {
+                        Should(ctx.tests).property('length', 2);
+                    });
+
+                    describe('the created suite', () => {
+                        it('should fail on the missing validator', () => {
+                            Should(ctx.failed)
+                                .be.an.Object();
+                            Should(ctx.failed.err)
+                                .have.property('message').match(/expected .+ to have type function/);
+                        });
+                    });
+                });
+
+                describe('and provided value has a valid validator function', () => {
+                    const factoryWithNonFuncValidate = (a, b) => ({ a, b });
+                    factoryWithNonFuncValidate.validate = opts => opts;
+
+                    const ctx = SUT(SUT.isExcoFactory).behavior({
+                        before: hijack,
+                        args: [factoryWithNonFuncValidate, { hasValidator: true }],
+                        ready: onReady,
+                    });
+
+                    it('should create two tests', () => {
+                        Should(ctx.tests).property('length', 2);
+                    });
+
+                    describe('the created suite', () => {
+                        it('should pass', () => {
+                            Should.not.exist(ctx.failed);
+                        });
+                    });
+                });
+            });
+
+            describe('when called with options.mapsEnv: true', () => {
+                describe('and provided value has no .env map', () => {
+                    const factoryWithNoEnvMap = (a, b) => ({ a, b });
+
+                    const ctx = SUT(SUT.isExcoFactory).behavior({
+                        before: hijack,
+                        args: [factoryWithNoEnvMap, { mapsEnv: true }],
+                        ready: onReady,
+                    });
+
+                    it('should create create additional test for the .env', () => {
+                        Should(ctx.tests).property('length', 2);
+                    });
+
+                    describe('the created suite', () => {
+                        it('should fail on the missing env map', () => {
+                            Should(ctx.failed)
+                                .be.an.Object();
+                            Should(ctx.failed.err)
+                                .have.property('message').match(/expected .+ to have property env/);
+                        });
+                    });
+                });
+
+                describe('and provided value has .env map', () => {
+                    const factoryWithEnvMap = (a, b) => ({ a, b });
+                    factoryWithEnvMap.env = {}
+
+                    const ctx = SUT(SUT.isExcoFactory).behavior({
+                        before: hijack,
+                        args: [factoryWithEnvMap, { mapsEnv: true }],
+                        ready: onReady,
+                    });
+
+                    it('should create additional test for the .env', () => {
+                        Should(ctx.tests).property('length', 2);
+                    });
+
+                    describe('the created suite', () => {
+                        it('should pass', () => {
+                            Should.not.exist(ctx.failed);
+                        });
+                    });
+                });
+            });
+
+            describe('when called with options.mapsEnv: <map> ', () => {
+                describe('and provided value has no .env map', () => {
+                    const factoryWithNoEnvMap = (a, b) => ({ a, b });
+                    factoryWithNoEnvMap.env = { prop1: 'ENV_PARAM1' };
+
+                    const ctx = SUT(SUT.isExcoFactory).behavior({
+                        before: hijack,
+                        args: [
+                            factoryWithNoEnvMap,
+                            {
+                                mapsEnv: ['prop1', 'prop2', 'prop3'],
+                            },
+                        ],
+                        ready: onReady,
+                    });
+
+                    it('should create create additional test for each key in mapsEnv', () => {
+                        Should(ctx.tests).property('length', 4);
+                    });
+
+                    describe('the created suite', () => {
+                        it('should fail on the first missing key', () => {
+                            Should(ctx.failed)
+                                .be.an.Object();
+                            Should(ctx.failed.err)
+                                .have.property('message').match(/expected .+ to have property prop2/);
+                        });
+                    });
+                });
+
+                describe('and provided value has .env map with all keys specified at .mapsEnv', () => {
+                    const factoryWithEnvMap = (a, b) => ({ a, b });
+                    factoryWithEnvMap.env = {
+                        prop1: 'ENV_PARAM1',
+                        prop2: 'ENV_PARAM2',
+                        prop3: 'ENV_PARAM3',
+                    };
+
+                    const ctx = SUT(SUT.isExcoFactory).behavior({
+                        before: hijack,
+                        args: [
+                            factoryWithEnvMap,
+                            {
+                                mapsEnv: ['prop1', 'prop2', 'prop3'],
+                            },
+                        ],
+                        ready: onReady,
+                    });
+
+                    it('should create two tests', () => {
+                        Should(ctx.tests).property('length', 4);
+                    });
+
+                    describe('the created suite', () => {
+                        it('should pass', () => {
+                            Should.not.exist(ctx.failed);
+                        });
+                    });
+                });
+            })
         });
 
         describe('.behavior({SUT}, spec)', () => {
